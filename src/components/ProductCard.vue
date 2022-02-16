6bafea0f9e84d6608626ea831f505d31f47f49f4
Julle <julius.emil.petersson@gmail.com>
2022-02-16 11:50:00 +0100

    implement api call and prod card

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,31 @@
+
+
+<template>
+  <div class="card">
+    <div>{{ name }}</div>
+    <div>{{ brand }}</div>
+    <button>köp</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductCard",
+  props: {
+    name: String,
+    brand: String,
+  },
+};
+</script>
+
+<style scoped>
+.card {
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 258px;
+  justify-content: space-evenly;
+  border-radius: 8px;
+}
+</style>
--- src/views/Kites.vue
+++ src/views/Kites.vue
@@ -1,16 +1,47 @@
 <template>
-    <div>
-        Alla våra kites
-    </div>
+  <div>
+    Alla våra kites
+    <div v-if="isLoading">...loading</div>
+    <ProductContainer class="productContainer">
+      <ProductCard
+        v-for="kite in kites"
+        :key="kite.id"
+        :brand="kite.model"
+        :name="kite.brand"
+      />
+    </ProductContainer>
+  </div>
 </template>
 
 <script>
+import ProductCard from "../components/ProductCard.vue";
 export default {
-  name: 'Kites'
-}
+  name: "Kites",
+  components: {
+    ProductCard,
+  },
+  data: function () {
+    return {
+      kites: [],
+      isLoading: true,
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/kites")
+      .then((response) => response.json())
+      .then((json) => {
+        this.kites = json;
+        this.isLoading = false;
+      })
+      .catch((e) => console.log("error", e));
+  },
+};
 </script>
 
 <style scoped>
-
-
+.productContainer {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 16px;
+}
 </style>
